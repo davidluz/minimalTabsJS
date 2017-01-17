02b7608cfa716bbfa0b69d842ba141c9f8df57c1
davidluz <davidmelo8@gmail.com>
2017-01-17 16:28:00 -0200

    v.0.0.1

---
+++ minimalTabsJS.js
@@ -0,0 +1,75 @@
+// V 1.0.0 
+
+$(document).ready(function(){
+
+
+
+$('#mt-menu').find( "li" ).eq(0).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(0).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(0).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(0).fadeIn();
+});	
+
+$('#mt-menu').find( "li" ).eq(1).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(1).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(1).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(1).fadeIn();
+});	
+
+$('#mt-menu').find( "li" ).eq(2).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(2).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(2).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(2).fadeIn();
+});	
+
+$('#mt-menu').find( "li" ).eq(3).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(3).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(3).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(3).fadeIn();
+});	
+
+$('#mt-menu').find( "li" ).eq(4).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(4).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(4).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(4).fadeIn();
+});	
+
+$('#mt-menu').find( "li" ).eq(5).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).css("background-color", "#b6b6b6");
+$('#mt-menu').find( "li" ).eq(5).css("background-color", "#004aa2");
+$('#mt-menu').find( "li" ).eq(5).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(5).fadeIn();
+});	
+
+
+$('#mt-menu').find( "li" ).eq(6).click(function(){
+$('#mt-contents').find( "li" ).hide();	
+$('#mt-menu').find( "li" ).css("opacity", "0.9");
+$('#mt-menu').find( "li" ).eq(6).css("opacity", "1.0");
+$('#mt-contents').find( "li" ).eq(6).fadeIn();
+});	
+
+
+
+$('#mt-menu').find( "li" ).eq(0).trigger( "click" );
+
+
+
+});
